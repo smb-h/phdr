6746dd6e91bed29eafc80b3e1f68233681a94964
SMB H <smbhop@pm.me>
2022-12-10 23:03:33 +0330

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='phdr',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Persian Handwritten Digit Recognition',
+    author='SMB H',
+    license='MIT',
+)
