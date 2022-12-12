d7d4cbe42808ed82979fc4e2bfbdb61329abe1a9
SMB H <smbhop@pm.me>
2022-12-12 18:15:52 +0330

    :zap: update(notebooks): minor changes

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='phdr',
+    name="phdr",
     packages=find_packages(),
-    version='0.1.0',
-    description='Persian Handwritten Digit Recognition',
-    author='SMB H',
-    license='MIT',
+    version="0.1.0",
+    description="Persian Handwritten Digit Recognition",
+    author="SMB H",
+    license="MIT",
 )
